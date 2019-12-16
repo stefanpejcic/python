9b21bceccd52b5f7188f8311dcc880a085c1f6bb
Stefan Pejcic <pejcha1994@gmail.com>
2019-12-16 06:31:39 +0100

    Create localize.py

---
+++ if/localize.py
@@ -0,0 +1,9 @@
+language = input("Language: ")
+
+if language == "rs":
+    print("Zdravo!")
+elif language == "de":
+    print("Hallo!")
+else:
+    print("Sorry, we don't support your language yet.")
+    print("Help us translate our website to your language: hello@localize.com")
