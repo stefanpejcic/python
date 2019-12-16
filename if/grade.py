db684ef4cd25f80af60ed7397513a459b2d6a471
Stefan Pejcic <pejcha1994@gmail.com>
2019-12-16 06:11:16 +0100

    Create grade.py

---
+++ if/grade.py
@@ -0,0 +1,13 @@
+grade = int(input('Grade: '))
+if(grade==1):
+    print("Worse")
+elif(grade==2):
+    print("Bad")
+elif(grade==3):
+    print("Average")
+elif(grade==4):
+    print("Good")
+elif(grade==5):
+    print("Excellent")
+else:
+    print("you call that grade?")
