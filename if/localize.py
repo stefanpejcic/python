ef9143642d7a133e3e4d1e4024b2e5f7f343721d
Stefan Pejcic <pejcha1994@gmail.com>
2019-12-16 12:02:04 +0100

    examples added

--- if/localize.py
+++ if/localize.py
@@ -1,9 +1,17 @@
+"""
+Get language input from a user, then greet him in that language.
+"""
+
 language = input("Language: ")
 
 if language == "rs":
     print("Zdravo!")
 elif language == "de":
     print("Hallo!")
+elif language == "en":
+    print("Hello!")
+elif language == "pl":
+    print("Witaj!")
 else:
     print("Sorry, we don't support your language yet.")
     print("Help us translate our website to your language: hello@localize.com")
