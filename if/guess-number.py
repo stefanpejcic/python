5e4aec2ffa4db47a45913c57500cae721a9253e9
Stefan Pejcic <pejcha1994@gmail.com>
2019-12-16 11:55:50 +0100

    Update guess-number.py

--- if/guess-number.py
+++ if/guess-number.py
@@ -1,5 +1,9 @@
+'''
+Get a random number from 1-10, then let user take a guess to gt a higher number.
+'''
+
 import random
-number      = random.randint(0,100)
+number      = random.randint(0,10)
 user_guess     = int(input("Enter your score: "))
 if(user_guess>number):
     print("Congrats! You beat the  high score!")
