772afec649e31e0a3e90c495b467fecd66d51c51
Stefan Pejcic <pejcha1994@gmail.com>
2019-12-16 06:03:27 +0100

    print if number is bellow or above 0

---
+++ if/zero.py
@@ -0,0 +1,8 @@
+number = int(input("Add your number: "))
+
+if(number>0):
+    print("Your number is positive")
+elif(number<0):
+    print("Your number is negative")
+else:
+    print("Bro, is that even a number?")
