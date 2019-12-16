d6f45112136acc020cb04d9ff6f2259883a90c9c
Stefan Pejcic <pejcha1994@gmail.com>
2019-12-16 11:38:14 +0100

    python 3.7.3

--- battleships.py
+++ battleships.py
@@ -7,9 +7,9 @@ for x in range(0,5):
 
 def print_board(board):
   for row in board:
-    print " ".join(row)
+    print .join(row)
 
-print "Let's play Battleship!"
+print ("Let's play Battleship!")
 print_board(board)
 
 def random_row(board):
@@ -20,29 +20,29 @@ def random_col(board):
 
 ship_row = random_row(board)
 ship_col = random_col(board)
-print ship_row
-print ship_col
+print (ship_row)
+print (ship_col)
 
 for turn in range(4):
 	guess_row = input("Guess Row:")
 	guess_col = input("Guess Col:")
 	
 	if guess_row == ship_row and guess_col == ship_col:
-	  print "Congratulations! You sunk my battleship!"
+	  print ("Congratulations! You sunk my battleship!")
 	  break
 	else:
 		if turn == 3:
 			board[guess_row][guess_col] = "X"
 			print_board(board)
-			print "Game Over"
-			print "My ship was here: [" + str(ship_row) + "][" + str(ship_col) + "]"
+			print ("Game Over")
+			print ("My ship was here: [" + str(ship_row) + "][" + str(ship_col) + "]")
 		else:			
 			if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
-				print "Oops, that's not even in the ocean."
+				print ("Oops, that's not even in the ocean.")
 			elif(board[guess_row][guess_col] == "X"):
-				print "You guessed that one already."
+				print ("You guessed that one already.")
 			else:
-				print "You missed my battleship!"
+				print ("You missed my battleship!")
 				board[guess_row][guess_col] = "X"
 			print (turn + 1)
 			print_board(board)
