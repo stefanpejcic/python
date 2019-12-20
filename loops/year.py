dd35b510b7711cd3e4a6a1f0bee7264b5d5925a4
Stefan Pejcic <pejcha1994@gmail.com>
2019-12-20 11:55:38 +0100

    Create year.py

---
+++ loops/year.py
@@ -0,0 +1,22 @@
+import time
+from = 1970
+to = 2000   
+print("*********** ALLOWED YEARS ************")
+for year in range(from,to+1):
+    print(year)
+print("**************************************")
+# import time
+# from = 1970
+# to = 2000 
+# while True: 
+#     f = open("index.html","w") 
+#     print("*********** ALLOWED YEARS ************")
+#     f.write("<select>")
+#     for year in range(from,to+1):
+#         f.write("<option>"+str(year)+"</option>")
+#     f.write("</select>")
+#     print("**************************************")
+#     f.close()
+#     from+=1
+#     to+=1
+#     time.sleep(10)
