f07d38b194c628dd22c3473a1b85aaf73ab5f618
Stefan Pejcic <pejcha1994@gmail.com>
2019-12-29 08:02:47 +0100

    Update email-scrape-page.py

--- advanced/email-scrape-page.py
+++ advanced/email-scrape-page.py
@@ -3,12 +3,26 @@ import re
 from bs4 import BeautifulSoup
 
 allLinks = [];mails=[]
-url = 'https://kore.ai/'
+url = input("Website: ")
+
+#check if its a url - start with http or www and is not a number
+if(url.startswith("http") or url.startswith("www")):
+  #if its a link check if its online
+  request = requests.get(url)
+  if request.status_code == 200:
+    pass
+  else:
+    print('Seems like the website is offline') 
+elif url.isdigit():
+  print("No IPs allowed, please add the website URL")
+else:
+        url="https://"+url
 response = requests.get(url)
 soup=BeautifulSoup(response.text,'html.parser')
 links = [a.attrs.get('href') for a in soup.select('a[href]') ]
+#define pages to look trough
 for i in links:
-    if(("contact" in i or "Contact")or("Career" in i or "career" in i))or('about' in i or "About" in i)or('Services' in i or 'services' in i):
+    if(("contact" in i or "Contact")or("Career" in i or "career" in i))or('about' in i or "About" in i or "ABOUT" in i)or('Services' in i or 'services' in i):
         allLinks.append(i)
 allLinks=set(allLinks)
 def findMails(soup):
@@ -22,6 +36,7 @@ def findMails(soup):
                 if(len(mails)==0)or(emailText not in mails):
                     print(emailText)
                 mails.append(emailText)
+#for inner pages
 for link in allLinks:
     if(link.startswith("http") or link.startswith("www")):
         r=requests.get(link)
@@ -39,3 +54,5 @@ for link in allLinks:
 mails=set(mails)
 if(len(mails)==0):
     print("NO MAILS FOUND")
+
+#custom error msgs?
