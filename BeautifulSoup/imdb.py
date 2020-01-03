5ee0f08bae9b200e68982fa02c34cfd0e46614ad
Stefan Pejcic <pejcha1994@gmail.com>
2020-01-03 11:15:49 +0100

    Update imdb.py

--- BeautifulSoup/imdb.py
+++ BeautifulSoup/imdb.py
@@ -2,7 +2,7 @@ import bs4
 import pandas as pd
 import requests
 
-url = 'https://www.imdb.com/search/title/?count=100&groups=top_1000&sort=user_rating'
+url = 'https://www.imdb.com/search/title/?groups=top_1000&sort=year,desc'
 def get_page_contents(url):
     page = requests.get(url, headers={"Accept-Language": "en-US"})
     return bs4.BeautifulSoup(page.text, "html.parser")
@@ -11,6 +11,8 @@ soup = get_page_contents(url)
 
 #extract movies
 movies   = soup.findAll('h3', class_='lister-item-header')
+
+'''
 titles   = [movie.find('a').text for movie in movies]
 release  = [movie.find('span', class_='lister-item-year text-muted unbold').text for movie in movies]
 
@@ -21,4 +23,6 @@ earnings = movie.findAll('span' , {'name' : 'nv'})[1]['data-value']
 
 director = movie.find('p').find('a').text
 actors   = [actor.text for actor in movie.find('p').findAll('a')[1:]]
+'''
 
+print (movies)
