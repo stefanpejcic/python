9725c4ea6eaa8d5656bb89ddd77f5dd97e756ef0
Stefan Pejcic <pejcha1994@gmail.com>
2020-01-08 10:41:19 +0100

    Update whois-list.py

--- advanced/whois-list.py
+++ advanced/whois-list.py
@@ -1,6 +1,7 @@
 import whois
 domeni = ["wporb.com","pluginsbay.com","pcelarstvopejcic.com","hostipay.com","filmovic.com","oklagije.com","giga.rs"]
-for domen in domeni:
-    w = whois.whois(domeni[0])
-    print(domeni[0], w.expiration_date)
+while True:
+    for domen in domeni:
+        w = whois.whois(domen)
+        print(domen, w.expiration_date)
 print()
