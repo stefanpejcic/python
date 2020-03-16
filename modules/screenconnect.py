a6c7f50912a9b1a4ab5bccf60e669a60e3a9db52
Stefan Pejcic <pejcha1994@gmail.com>
2020-03-16 09:58:13 +0100

    Update screenconnect.py

--- modules/screenconnect.py
+++ modules/screenconnect.py
@@ -1,5 +1,16 @@
 import screenconnect
 sc = screenconnect.ScreenConnect('http://nes.to/', auth=('user', 'pass'))
 
-sc.server_version
-sc.get_eligible_hosts()
+#print("Verzija:"sc.server_version)
+
+#print (sc.get_eligible_hosts())
+
+#print (sc.get_toolbox())
+
+#print(sc.get_eligible_hosts())
+
+#print(sc.get_session_report())
+
+# print(sc.get_host_session_info())
+
+#print(sc.get_guest_session_info())
